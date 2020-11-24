6d12c32a43a0f42a161d49985e291a525ae8cb6b
Samitha <rmschathuranga@gmail.com>
2020-11-24 12:46:04 +0000

    changed

--- resources/views/backend/department/view.blade.php
+++ resources/views/backend/department/view.blade.php
@@ -23,7 +23,7 @@
         <div class="col-md-12">
 <div class="box">
             <div class="box-header with-border">
-              <h3 class="box-title">View Department h</h3>
+              <h3 class="box-title">View Department</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">

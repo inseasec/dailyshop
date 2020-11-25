c64f7a25de6a38744af164a06a846963b7276ca3
Samitha <rmschathuranga@gmail.com>
2020-11-25 12:40:50 +0000

    changed title

--- resources/views/frontend/layout/header_includes.blade.php
+++ resources/views/frontend/layout/header_includes.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">    
-    <title>Daily Shop | Home changed </title>
+    <title>Seasec </title>
     
     <!-- Font awesome -->
     <!-- <link href="{{URL::asset('js/scrollTo.js')}}" rel="stylesheet"> -->
